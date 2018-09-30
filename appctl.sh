64379504053d11f91c00f5421f237df611557e46
uibrick <uibrick@126.com>
2018-09-30 10:36:57 +0800

    ，。

---
+++ appctl.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cp -f ./run.sh /etc/cron.daily/asset-trade-service.sh
+
+source run.sh
+
+function build()
+{
+    echo "try to build image: <$IMAGE>";
+    docker build -t "$IMAGE" "$WD"
+    echo "build <$IMAGE> done";
+}
+
+if [[ "$1" == "start" ]]; then
+build;
+start;
+elif [[ "$1" == "stop" ]]; then
+stop;
+fi
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+IMAGE="asset-trade-service"
+WD="/home/rdc/asset-trade"
+
+function start()
+{
+    echo "start ...";
+    docker run -d -e PRODUCTION=1 -e TZ="Asia/Shanghai" --rm --name "$IMAGE" -v "$WD":/usr/src/app \
+        -w /usr/src/app -p 5000:5000 asset-trade-service python app.py;
+    echo "started";
+}
+
+function stop()
+{
+    echo "stop ...";
+    docker stop "$IMAGE";
+    echo "stoped";
+}
+
